b160146a31ca9be7937f238d59fa11bdb8bf7243
PedroMurilloArnau <Pedro.marnau@gmail.com>
2021-10-16 20:50:41 +0200

    Implementando el menu

---
+++ JS/menu.js
@@ -0,0 +1,17 @@
+
+function menu(){
+    let menu = 
+    "<input type='button' id='btn_prueba' onclick='prueba()' value='Formulario'/>"+
+    "<div id='div2' align='center'>"+
+    "</div>"
+
+
+    div1.innerHTML = menu
+}
+//que vevera ul y luego un li
+function prueba(){
+    let forml =
+    "<p>Prueba de cosas</p>"
+    div2.innerHTML = forml
+    
+}
